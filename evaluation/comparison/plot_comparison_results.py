3496a6d7e1055486d394a4a12e9ed0c8c87d7a26
ehuangc <edwardhuangc@gmail.com>
2026-03-24 21:34:09 +0100

    Change memory plot y-axis units to GB

--- evaluation/comparison/plot_comparison_results.py
+++ evaluation/comparison/plot_comparison_results.py
@@ -16,10 +16,10 @@ def seconds_to_minutes(df: pd.DataFrame) -> pd.DataFrame:
     return df
 
 
-def bytes_to_mb(df: pd.DataFrame) -> pd.DataFrame:
+def bytes_to_gb(df: pd.DataFrame) -> pd.DataFrame:
     df = df.copy()
-    df["mean_mb"] = df["mean_bytes"] / (1024**2)
-    df["stddev_mb"] = df["stddev_bytes"] / (1024**2)
+    df["mean_gb"] = df["mean_bytes"] / (1024**3)
+    df["stddev_gb"] = df["stddev_bytes"] / (1024**3)
     return df
 
 
@@ -36,11 +36,15 @@ def format_duration(seconds: float) -> str:
         return f"{hrs}hr {mins}min"
 
 
-def format_memory(mb: float) -> str:
-    if mb >= 1024:
-        return f"{mb / 1024:.2f} GB"
+def format_memory(bytes: float) -> str:
+    gb = bytes / (1024**3)
+    mb = bytes / (1024**2)
+    if gb >= 1:
+        return f"{gb:.2f} GB"
+    elif mb >= 1:
+        return f"{mb:.0f} MB"
     else:
-        return f"{mb:.2f} MB"
+        return f"{bytes:.0f} B"
 
 
 def save_bar_plot(
@@ -52,7 +56,9 @@ def save_bar_plot(
     title: str,
     output_path: Path,
     seconds_col: str | None = None,
+    bytes_col: str | None = None,
 ) -> None:
+    assert seconds_col or bytes_col
     fig, ax = plt.subplots(figsize=(5, 5), constrained_layout=True)
     ax.bar(
         data[label_col],
@@ -64,7 +70,10 @@ def save_bar_plot(
     )
     y_max = (data[y_col] + data[err_col]).max()
     for x, (y, err) in enumerate(zip(data[y_col], data[err_col], strict=True)):
-        ann = format_duration(data[seconds_col].iloc[x]) if seconds_col else format_memory(y)
+        if seconds_col:
+            ann = format_duration(data[seconds_col].iloc[x])
+        else:
+            ann = format_memory(data[bytes_col].iloc[x])
         ax.text(
             x,
             y + err + y_max * 0.01,
@@ -90,7 +99,7 @@ def main() -> None:
     PLOTS_DIR.mkdir(parents=True, exist_ok=True)
 
     readv2_df = pd.read_csv(READV2_CSV)
-    readv2_df = bytes_to_mb(readv2_df)
+    readv2_df = bytes_to_gb(readv2_df)
     readv2_df = seconds_to_minutes(readv2_df)
     save_bar_plot(
         readv2_df,
@@ -105,11 +114,12 @@ def main() -> None:
     save_bar_plot(
         readv2_df,
         "label",
-        "mean_mb",
-        "stddev_mb",
-        "Peak RSS (MB)",
+        "mean_gb",
+        "stddev_gb",
+        "Peak RSS (GB)",
         "Peak Memory: fastpmr vs. READv2",
         PLOTS_DIR / "readv2_comparison_benchmark_memory.pdf",
+        bytes_col="mean_bytes",
     )
 
 
